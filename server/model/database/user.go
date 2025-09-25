d5e83ee3ee67a0530acd33f754d46074bc2588ce
Lonely_Snow <3200975318@qq.com>
2025-09-25 18:33:11 +0800

    database

---
+++ server/model/database/user.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"github.com/gofrs/uuid"
+	"server/model/appTypes"
+)
+
+type User struct {
+	UUID      uuid.UUID         `json:"uuid" gorm:"type:char(36);unique"`
+	Username  string            `json:"username"`
+	Password  string            `json:"password"`
+	Email     string            `json:"email"`
+	Openid    string            `json:"openid"`
+	Avatar    string            `json:"avatar" gorm:"size:255"`
+	Address   string            `json:"address"`
+	Signature string            `json:"signature" gorm:"default:'这里怎么什么都没有喵？'"`
+	RoleID    appTypes.RoleID   `json:"role_id"`
+	Register  appTypes.Register `json:"register"`
+	Freeze    bool              `json:"freeze"`
+}
