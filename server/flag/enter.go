b776ebedb5285d221a582f83e30ed848d9f05cfe
Lonely_Snow <3200975318@qq.com>
2025-09-29 18:27:18 +0800

    SQL export

--- server/flag/enter.go
+++ server/flag/enter.go
@@ -13,6 +13,10 @@ var (
 		Name:  "sql",
 		Usage: "Initializes the structure of the database table",
 	}
+	sqlExportFlag = &cli.BoolFlag{
+		Name:  "sql-export",
+		Usage: "Exports SQL data to a specified file.",
+	}
 )
 
 func Run(c *cli.Context) {
@@ -23,12 +27,18 @@ func Run(c *cli.Context) {
 		os.Exit(1)
 	}
 	switch {
-	case c.Bool("sql"):
+	case c.Bool(sqlFlag.Name):
 		if err := SQL(); err != nil {
 			global.Log.Error("Failed to initialize database structure:", zap.Error(err))
 			return
 		} else {
 			global.Log.Info("Successful database structure initialized")
+		}
+	case c.Bool(sqlExportFlag.Name):
+		if err := SQLExport(); err != nil {
+			global.Log.Error("Failed to export SQL data:", zap.Error(err))
+		} else {
+			global.Log.Info("Successfully exported SQL data")
 		}
 	default:
 		err := cli.NewExitError("Unknow command", 1)
@@ -41,6 +51,7 @@ func NewApp() *cli.App {
 	app.Name = "go_blog"
 	app.Flags = []cli.Flag{
 		sqlFlag,
+		sqlExportFlag,
 	}
 	app.Action = Run
 	return app
