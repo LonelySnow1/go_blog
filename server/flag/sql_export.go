d26ed95154be713693d88c445f0813d051702050
Lonely_Snow <3200975318@qq.com>
2025-09-30 18:00:47 +0800

    SQL import

--- server/flag/sql_export.go
+++ server/flag/sql_export.go
@@ -14,7 +14,8 @@ func SQLExport() error {
 
 	timer := time.Now().Format("20060102")
 	sqlPath := fmt.Sprintf("mysql_%s.sql", timer)
-	cmd := exec.Command("docker", "exec", "mysql", "mysqldump", "-u"+mysqlCfg.Username, "-p"+mysqlCfg.Password, mysqlCfg.DBName)
+	cmd := exec.Command("docker", "exec", "mysql", "mysqldump", "-u"+mysqlCfg.Username, "-p"+mysqlCfg.Password, mysqlCfg.DBName) // 本地运行
+	//cmd := exec.Command("ssh", "ubuntu@203.0.113.5", "docker exec mysql mysqldump -u"+mysqlCfg.Username+" -p"+mysqlCfg.Password+" "+mysqlCfg.DBName)	 //服务器运行示例
 
 	outFile, err := os.Create(sqlPath)
 	if err != nil {
