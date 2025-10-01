d23ace3a03948cf32c4f13984222b0f51baabb9c
Lonely_Snow <3200975318@qq.com>
2025-10-01 17:16:12 +0800

    es

---
+++ server/service/enter.go
@@ -0,0 +1,14 @@
+package service
+
+/*
+ 为什么要这样设计：
+	1. 服务集中管理（Service Group 模式）：将相关业务集成到服务组中，方便管理
+	2. 单例复用，避免资源浪费：ServiceGroupApp是一个全局单例，内部的示例会被复用
+	3. 代码分层与职责边界：通过入口调用服务，而不是直接依赖具体的服务实现。 符合「依赖倒置原则」,方便后续对具体实现进行修改
+*/
+
+type ServiceGroup struct {
+	EsService
+}
+
+var ServiceGroupApp = new(ServiceGroup)
--- server/service/es_index.go
+++ server/service/es_index.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
+	"server/global"
+)
+
+type EsService struct{}
+
+// IndexCreate 创建索引
+func (esService *EsService) IndexCreate(indexName string, mapping *types.TypeMapping) error {
+	_, err := global.ESClient.Indices.Create(indexName).Mappings(mapping).Do(context.TODO())
+	return err
+}
+
+// IndexDelete 删除索引
+func (esService *EsService) IndexDelete(indexName string) error {
+	_, err := global.ESClient.Indices.Delete(indexName).Do(context.TODO())
+	return err
+}
+
+// IndexExist 判断索引是否存在
+func (esService *EsService) IndexExist(indexName string) (bool, error) {
+	return global.ESClient.Indices.Exists(indexName).Do(context.TODO())
+}
