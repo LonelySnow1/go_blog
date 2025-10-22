02a156522ef0a6cf2ba8cbf039b5d783a25c4da9
Lonely_Snow <3200975318@qq.com>
2025-10-22 15:42:05 +0800

    article router

--- server/service/enter.go
+++ server/service/enter.go
@@ -1,12 +1,5 @@
 package service
 
-/*
- 为什么要这样设计：
-	1. 服务集中管理（Service Group 模式）：将相关业务集成到服务组中，方便管理
-	2. 单例复用，避免资源浪费：ServiceGroupApp是一个全局单例，内部的示例会被复用
-	3. 代码分层与职责边界：通过入口调用服务，而不是直接依赖具体的服务实现。 符合「依赖倒置原则」,方便后续对具体实现进行修改
-*/
-
 type ServiceGroup struct {
 	EsService
 	BaseService
@@ -15,6 +8,7 @@ type ServiceGroup struct {
 	UserService
 	QQService
 	ImageService
+	ArticleService
 }
 
 var ServiceGroupApp = new(ServiceGroup)
